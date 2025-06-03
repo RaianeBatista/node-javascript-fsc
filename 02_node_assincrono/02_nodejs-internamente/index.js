d88f83ccbaed052be9d6bfbfbb5ffdaf4d5a46c6
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-06-03 17:25:41 -0300

    feat: lession Node.js assincrono

---
+++ 02_node_assincrono/02_nodejs-internamente/index.js
@@ -0,0 +1,15 @@
+console.log("Iniciando...");
+
+setTimeout(() => {
+  console.log("Temporizador de 5 segundos!");
+}, 5000);
+
+setTimeout(() => {
+  console.log("Temporizador de 2 segudnos");
+}, 2000);
+
+setTimeout(() => {
+  console.log("Temporizador de 0 segudnos");
+}, 0);
+
+console.log("Finalizando...");
