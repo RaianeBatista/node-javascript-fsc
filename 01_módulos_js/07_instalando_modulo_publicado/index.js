83f696d37c9cb912b635d3c76c316a2dba36fdff
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-06-03 16:47:49 -0300

    feat: add publishing module on npm

---
+++ 01_módulos_js/07_instalando_modulo_publicado/index.js
@@ -0,0 +1,35 @@
+import { sayHello, sum } from "@raianebatista/fullstack";
+
+sayHello();
+const result = sum(4, 4);
+
+console.log(result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+);
---
+++ 02_node_assincrono/01_entendendo_codigo_assicrono/index.js
@@ -0,0 +1,8 @@
+console.log("Primeira linha");
+// console.log("Segunda linha")
+setTimeout(() => {
+  console.log("Segunda linha"); // Código assíncrono
+}, 5000);
+console.log("Terceira linha");
+
+
